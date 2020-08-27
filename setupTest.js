1af5a0db9cf10784a605694da96a919810ca215e
hurcy <cinyoung.hur@gmail.com>
2020-08-27 13:25:35 +0900

    chore: remove test setting file

--- setupTest.js
+++
@@ -1,6 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import 'mutationobserver-shim';
-import "@testing-library/jest-dom/extend-expect";
-
-configure({ adapter: new Adapter() });
